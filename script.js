ffbf23cce8f49163a409bd8b786d4164f60859c3
Mr. Prince <136004067+RoyalPrincely@users.noreply.github.com>
2025-06-15 13:52:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ window.onload = function () {
   // Log a message when the entire website finishes loading
   console.log("Website has been loaded successfully!");
 }
-
-smartlook('record', { forms: true, numbers: true, emails: true, ips: true })
