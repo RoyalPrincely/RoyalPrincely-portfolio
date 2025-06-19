56ee7ddf0debc4f43d9a22087b5aca7d15398f82
Mr. Prince <136004067+RoyalPrincely@users.noreply.github.com>
2025-06-19 19:51:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ window.onload = function () {
   // Log a message when the entire website finishes loading
   console.log("Website has been loaded successfully!");
 }
-
-javascript:(function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })();
